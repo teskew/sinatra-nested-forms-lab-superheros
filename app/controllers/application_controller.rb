6b9c770b3f3d82e0e61ba2ef04bd8a5636f915ad
teskew <teskinder1@gmail.com>
2021-02-13 11:09:59 -0600

    commit tree 33335d47dec275d34f58c55cef8544086173a04c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,30 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+      end
+  
+  
+    post '/teams' do
+        @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+        members = params[:team][:members]
+  
+           members.each do |member_params|
+          SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+           end
+        @super_heroes = SuperHero.all
+  
+        erb :team
+      end
+  end	
 
-end
+  
+  #or can write it the post this way
+  #@team = Team.new(params[:team])
+  #params[:team][:members].each do |member_params|
+  #  SuperHero.new(member_params)
+  #end
+  #@super_heroes = SuperHero.all
+
+
---
+++ app/models/team.rb
@@ -0,0 +1,8 @@
+class Team
+    attr_reader :name, :motto
+  
+    def initialize(params)
+      @name = params[:name]
+      @motto = params[:motto]
+    end
+  end
